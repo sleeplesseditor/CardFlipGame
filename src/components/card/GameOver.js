ea53527c4d670a35a2dfa1af30d744cbd1ccd44f
sleeplesseditor <josh@bryson.me.uk>
2018-12-05 16:14:51 +0000

    Header and Game Over component setup

---
+++ src/components/card/GameOver.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const GameOver = ({ restartGame }) => (
+    <div className="justify-center">
+        <h1>Game Over</h1>
+        <h3>Try Again!</h3>
+        <button
+            className="restart-button"
+            onClick={restartGame}
+        >
+            Restart Game
+        </button>
+    </div>
+);
+
+export default GameOver;
